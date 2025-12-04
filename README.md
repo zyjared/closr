a9273d4761d3d7bdba3fa004095f0b348ff91c91
zyjared <zyjared@outlook.com>
2025-12-04 23:53:58 +0800

    docs: update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # closr
+
+## 使用
+
+pnpm 运行：
+
+```bash
+pnpm run dev
+```
+
+npm 运行
+
+```bash
+npm run dev
+```
--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     },
   },
   server: {
-    open: false
+    // open: false
   },
   plugins: [
     // https://github.com/posva/unplugin-vue-router
